c5af241e3095727317bb5b2dfae32fc3be817d25
Artur Upton Renault <artur.renault@gmail.com>
2015-03-31 04:08:47 +0000

    Basic AST API

---
+++ include/ast.cpp
@@ -0,0 +1,24 @@
+class ASTNode {
+    virtual ~ASTNode() = default;
+}
+
+class ExpressionNode : ASTNode {
+}
+
+class IntNode : ExpressionNode {
+    public:
+        long value;
+        IntNode(const long v): value(v) { }
+}
+
+class FloatNode : ExpressionNode {
+    public:
+        double value;
+        FloatNode(const double v): value(v) { }
+}
+
+class StringNode : ExpressionNode {
+    public:
+        std::string value;
+        StringNode(const std::string v): value(v) { } 
+}
