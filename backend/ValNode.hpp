55683f685c57b4dd88beab916fa8ecc7b8feda45
Alexander Roth <aisaacroth@gmail.com>
2015-04-03 03:11:32 +0000

    Finished the ValNode class

--- backend/ValNode.hpp
+++ backend/ValNode.hpp
@@ -1,5 +1,11 @@
 #include <list>
 
+/**
+ * ValNode.hpp - Templated value node to create the dependency tree.
+ *
+ * Author: Alexander Roth
+ * Date:   2015-04-02
+ **/
 using namespace std;
 
 struct Variant {
@@ -26,15 +32,67 @@ class ValNode {
         T value;
         V& function;
         list<Variant> arguments;
-        list<ValNode> children;
+        list<ValNode> children {nullptr};
     public:
         ValNode(T val, V& func, list<Variant> args);
-        void addNode();
+        ~ValNode();
+        void addChildNode(ValNode<T, V> &node);
+
         T getVal();
         list<ValNode> getChildren();
-        ValNode getChild(T val);
+        list<Variant> getArguments();
+
         void setVal(T val);
-        void setLeft(ValNode &left);
-        void setRight(ValNode &right);
-        ~ValNode();
+        void setChildren(list<ValNode> children);
+        void setArguments(list<Variant> arguments);
 };
+
+template<typename T, typename V>
+ValNode<T, V>::ValNode(T val, V& func, list<Variant> args) {
+    this->value = val;
+    this->function = func;
+    this->arguments = args;
+}
+
+template<typename T, typename V>
+ValNode<T, V>::~ValNode() {
+    this->value = nullptr;
+    this->function = nullptr;
+    this->arguments = nullptr;
+    this->children = nullptr;
+}
+
+template<typename T, typename V>
+void ValNode<T, V>::addChildNode(ValNode &node) {
+    this->children.push_back(node);
+}
+
+template<typename T, typename V>
+T ValNode<T, V>::getVal() {
+    return this->value;
+}
+
+template<typename T, typename V>
+list<typename ValNode<T,V>::ValNode> ValNode<T, V>::getChildren() {
+    return this->children;
+}
+
+template<typename T, typename V>
+list<Variant> ValNode<T, V>::getArguments() {
+    return this->arguments;
+}
+
+template<typename T, typename V>
+void ValNode<T, V>::setVal(T val) {
+    this->value = val;
+}
+
+template<typename T, typename V>
+void ValNode<T, V>::setChildren(list<ValNode> children) {
+    this->children = children;
+}
+
+template<typename T, typename V>
+void ValNode<T, V>::setArguments(list<Variant> arguments) {
+    this->arguments = arguments;
+}
